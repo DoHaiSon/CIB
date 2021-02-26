d55f133b76a43cc2aeb5afa8a8adcaf937b6f94c
dohaison <dohaison1998@gmail.com>
2021-02-26 09:24:11 +0700

    fixed python3

--- kdd_distributed3_run.py
+++ kdd_distributed3_run.py
@@ -9,7 +9,7 @@ if os.name == 'nt': # Windows
         subprocess.Popen('python kdd_distributed3.py --job_name "worker" --task_index 2', shell = True, stderr=log_w2)
 else:   #Linux
     with open("log_ps0.txt", "w") as log_ps0, open("log_w0.txt", "w") as log_w0, open("log_w1.txt", "w") as log_w1, open("log_w2.txt", "w") as log_w2:
-        subprocess.Popen('python kdd_distributed3.py --job_name "ps" --task_index 0', shell = True, stderr=log_ps0)
-        subprocess.Popen('python kdd_distributed3.py --job_name "worker" --task_index 0', shell = True, stderr=log_w0)
-        subprocess.Popen('python kdd_distributed3.py --job_name "worker" --task_index 1', shell = True, stderr=log_w1)
-        subprocess.Popen('python kdd_distributed3.py --job_name "worker" --task_index 2', shell = True, stderr=log_w2)
+        subprocess.Popen('python3 kdd_distributed3.py --job_name "ps" --task_index 0', shell = True, stderr=log_ps0)
+        subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 0', shell = True, stderr=log_w0)
+        subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 1', shell = True, stderr=log_w1)
+        subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 2', shell = True, stderr=log_w2)
