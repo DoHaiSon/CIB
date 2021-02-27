d4fbc331db0e59a5d2b1409f09cf95b05a0f1b63
dohaison1998 <dohaison1998@gmail.com>
2021-02-27 18:18:05 +0700

    add force quit program in Linux

--- kdd_distributed3_run.py
+++ kdd_distributed3_run.py
@@ -13,3 +13,6 @@ else:   #Linux
         subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 0', shell = True, stderr=log_w0)
         subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 1', shell = True, stderr=log_w1)
         subprocess.Popen('python3 kdd_distributed3.py --job_name "worker" --task_index 2', shell = True, stderr=log_w2)
+        kill = input('Stop? (y/Y): ')
+        if kill == 'y' or kill == 'Y':
+               subprocess.Popen('pkill -9 -f python3', shell = True)
