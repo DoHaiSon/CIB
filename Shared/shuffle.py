578d08cac9fb107822d7ff674e7f8481348c645a
dohaison <dohaison1998@gmail.com>
2021-04-20 11:05:11 +0700

    add more normal state in dataset.

--- Shared/shuffle.py
+++ Shared/shuffle.py
@@ -1,11 +1,11 @@
 from sklearn.utils import shuffle
 import pandas as pd 
 
-train = pd.read_csv(r"C:\Users\dohai\Desktop\train.csv", header=None) 
-test = pd.read_csv(r"C:\Users\dohai\Desktop\test.csv", header=None) 
+train = pd.read_csv("/home/ssllap1/Desktop/CIB/datasets/our_kdd_99/train_W1.csv", header=None) 
+test = pd.read_csv("/home/ssllap1/Desktop/CIB/datasets/our_kdd_99/train_W2.csv", header=None) 
 
 train_shuffled = shuffle(train)
 test_shuffled = shuffle(test)
 
-train_shuffled.to_csv(r'C:\Users\dohai\Desktop\CIB\datasets\our_kdd_99\train.csv', index = False, header=None)
-test_shuffled.to_csv(r'C:\Users\dohai\Desktop\CIB\datasets\our_kdd_99\test.csv', index = False, header=None)
+train_shuffled.to_csv("/home/ssllap1/Desktop/CIB/datasets/our_kdd_99/train_W1.csv", index = False, header=None)
+test_shuffled.to_csv("/home/ssllap1/Desktop/CIB/datasets/our_kdd_99/train_W2.csv", index = False, header=None)
