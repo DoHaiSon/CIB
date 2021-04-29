172020628ee4c1b09aea4d43f78c39836ee6f4a6
dohaison <dohaison1998@gmail.com>
2021-04-29 18:47:00 +0700

    fixed error ValueError: Found array with 0 sample(s)

--- Shared/collect_data.py
+++ Shared/collect_data.py
@@ -8,7 +8,7 @@ from Shared.read_dataset import read_dataset
 def collect_dataset():
     while(True):
         try:
-            raw_file = pd.read_csv("/home/iot-nexcom/CIB/datasets/our_kdd_99/data_raw.csv", header=None)
+            raw_file = read_data("/home/iot-nexcom/CIB/datasets/our_kdd_99/data_raw.csv")
             if (len(raw_file) > 0):
                 break
         except:
@@ -16,4 +16,15 @@ def collect_dataset():
     # # Remove 5 last columns in raw dataset
     # raw_file.drop(raw_file.columns[[-1, -2, -3, -4, -5]], axis=1, inplace=True)
     # raw_file.to_csv("/home/iot-nexcom/CIB/datasets/our_kdd_99/data_raw.csv", index = False, header=None)
-    return(read_dataset("/home/iot-nexcom/CIB/datasets/our_kdd_99/data_raw.csv"))
+    return(read_dataset(raw_file))
+
+def read_data(filename):
+    col_names = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes",
+                "land", "wrong_fragment", "urgent", "count", "srv_count", "serror_rate",
+                "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", 
+                "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
+                "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
+                "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", 
+                "dst_host_rerror_rate", "dst_host_srv_rerror_rate"]
+    dataset = pd.read_csv(filename, header = None, names = col_names)
+    return dataset      
--- Shared/read_dataset.py
+++ Shared/read_dataset.py
@@ -30,7 +30,7 @@ import os
 import pandas as pd
 
 def read_dataset(raw_file):
-    test_dataset = read_data(raw_file)
+    test_dataset = raw_file
 
     nomial_test(test_dataset)
     num_features = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes",
@@ -120,16 +120,7 @@ def segment_signal(data, window_size = 1):
 
     return segments
 
-def read_data(filename):
-    col_names = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes",
-                "land", "wrong_fragment", "urgent", "count", "srv_count", "serror_rate",
-                "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", 
-                "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
-                "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
-                "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", 
-                "dst_host_rerror_rate", "dst_host_srv_rerror_rate"]
-    dataset = pd.read_csv(filename, header = None, names = col_names)
-    return dataset      
+
 
 def normalize(dataset):
     mu = np.mean(dataset, axis = 0)
