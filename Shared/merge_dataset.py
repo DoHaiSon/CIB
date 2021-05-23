833accbfdda0e0859079bab007509c168b15a4c4
dohaison <dohaison1998@gmail.com>
2021-05-24 00:31:26 +0700

    new dataset

--- Shared/merge_dataset.py
+++ Shared/merge_dataset.py
@@ -5,15 +5,15 @@ import sys
 from sklearn.utils import shuffle
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))   # Join parent path to import library
-# os.chdir(r"datasets\our_kdd_99\mirai")
+# os.chdir(r"datasets\our_kdd_99\dos")
 
 # extension = 'csv'
 # all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 # #combine all files in the list
 # combined_csv = pd.concat([pd.read_csv(f, header=None) for f in all_filenames ])
 # # #export to csv
-# combined_csv.to_csv(r"C:\Users\dohai\Desktop\CIB\datasets\our_kdd_99\data\mirai_1.csv", header=None, index=False)
-train = pd.read_csv(r"datasets\our_kdd_99\data\train_W2_droped.csv", header=None)
+# combined_csv.to_csv(r"C:\Users\dohai\Desktop\CIB\datasets\our_kdd_99\data\dos_1.csv", header=None, index=False)
+train = pd.read_csv(r"datasets\our_kdd_99\data\train_W2.csv", header=None)
 print(train)
 # train_droped = train.drop_duplicates(keep="first") 
 train_droped = shuffle(train)
